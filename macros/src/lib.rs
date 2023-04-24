58ad810bd07d1b6fb18964b07844296b2277091b
Sam Johnson <sam@durosoft.com>
2023-04-24 16:01:48 -0400

    basic parsing of DeriveParseAttr

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,124 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{
+    bracketed, parenthesized,
+    parse::Nothing,
+    parse2,
+    token::{Bracket, Paren},
+    Error, Expr, Ident, ItemEnum, ItemStruct, Result, Token,
+};
 
+fn _derive_parse(tokens: impl Into<TokenStream2>) -> Result<TokenStream2> {
+    let tokens = tokens.into();
+    if let Ok(item_struct) = parse2::<ItemStruct>(tokens.clone()) {
+        return derive_parse_struct(item_struct);
+    }
+    let item_enum = parse2::<ItemEnum>(tokens)?;
+    derive_parse_enum(item_enum)
+}
+
+mod keywords {
+    use syn::custom_keyword;
+
+    custom_keyword!(brace);
+    custom_keyword!(bracket);
+    custom_keyword!(paren);
+    custom_keyword!(inside);
+    custom_keyword!(call);
+    custom_keyword!(parse_if);
+    custom_keyword!(prefix);
+    custom_keyword!(postfix);
+}
+
+enum DeriveParseAttr {
+    Brace,
+    Bracket,
+    Paren,
+    Inside(Ident),
+    Call(Expr),
+    ParseIf(Expr),
+    Prefix(Expr),
+    Postfix(Expr),
+}
+
+impl syn::parse::Parse for DeriveParseAttr {
+    fn parse(input: syn::parse::ParseStream) -> Result<Self> {
+        let content;
+        let _pound_token: Token![#] = input.parse()?;
+        let _bracket_token: Bracket = bracketed!(content in input);
+        if content.peek(keywords::brace) {
+            content.parse::<keywords::brace>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Brace)
+        } else if content.peek(keywords::bracket) {
+            content.parse::<keywords::bracket>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Bracket)
+        } else if content.peek(keywords::paren) {
+            content.parse::<keywords::paren>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Paren)
+        } else if content.peek(keywords::inside) {
+            content.parse::<keywords::inside>()?;
+            let paren_content;
+            let _paren_token: Paren = parenthesized!(paren_content in content);
+            let ident = paren_content.parse::<Ident>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Inside(ident))
+        } else if content.peek(keywords::call) {
+            content.parse::<keywords::call>()?;
+            let paren_content;
+            let _paren_token: Paren = parenthesized!(paren_content in content);
+            let expr = paren_content.parse::<Expr>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Call(expr))
+        } else if content.peek(keywords::parse_if) {
+            content.parse::<keywords::parse_if>()?;
+            let paren_content;
+            let _paren_token: Paren = parenthesized!(paren_content in content);
+            let expr = paren_content.parse::<Expr>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::ParseIf(expr))
+        } else if content.peek(keywords::prefix) {
+            content.parse::<keywords::prefix>()?;
+            let paren_content;
+            let _paren_token: Paren = parenthesized!(paren_content in content);
+            let expr = paren_content.parse::<Expr>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Prefix(expr))
+        } else if content.peek(keywords::postfix) {
+            content.parse::<keywords::postfix>()?;
+            let paren_content;
+            let _paren_token: Paren = parenthesized!(paren_content in content);
+            let expr = paren_content.parse::<Expr>()?;
+            content.parse::<Nothing>()?;
+            Ok(DeriveParseAttr::Postfix(expr))
+        } else {
+            return Err(Error::new(
+                content.span(),
+                "Expected one of `brace`, `bracket`, `paren`,\
+                `inside`, `call`, `parse_if`, `prefix`, `postfix`.",
+            ));
+        }
+    }
+}
+
+fn derive_parse_struct(_item_struct: ItemStruct) -> Result<TokenStream2> {
+    Ok(quote!())
+}
+
+fn derive_parse_enum(_item_enum: ItemEnum) -> Result<TokenStream2> {
+    Ok(quote!())
+}
+
+#[proc_macro_derive(Parse)]
+pub fn derive_parse(tokens: TokenStream) -> TokenStream {
+    match _derive_parse(tokens) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
+
+#[cfg(test)]
+mod tests;
---
+++ macros/src/tests.rs
@@ -0,0 +1,151 @@
+use super::*;
+
+#[test]
+fn derive_parse_attr_bracket_valid() {
+    let tokens = quote!(#[bracket]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Bracket,
+    ));
+}
+
+#[test]
+fn derive_parse_attr_bracket_invalid() {
+    let tokens = quote!(#[bracket = something]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_paren_valid() {
+    let tokens = quote!(#[paren]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Paren,
+    ));
+}
+
+#[test]
+fn derive_parse_attr_paren_invalid() {
+    let tokens = quote!(#[paren(something)]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_brace_valid() {
+    let tokens = quote!(#[brace]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Brace,
+    ));
+}
+
+#[test]
+fn derive_parse_attr_brace_invalid() {
+    let tokens = quote!(#[brace!]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_inside_valid() {
+    let tokens = quote!(#[inside(some_ident)]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Inside(_),
+    ));
+}
+
+#[test]
+fn derive_parse_attr_inside_invalid() {
+    let tokens = quote!(#[inside[some_ident]]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[inside some_ident]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[inside]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[inside()]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_call_valid() {
+    let tokens = quote!(#[call(some_ident.some_func())]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Call(_),
+    ));
+}
+
+#[test]
+fn derive_parse_attr_call_invalid() {
+    let tokens = quote!(#[call[something]]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[call some_ident]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[call]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[call()]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_parse_if_valid() {
+    let tokens = quote!(#[parse_if(some_ident.some_func())]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::ParseIf(_),
+    ));
+}
+
+#[test]
+fn derive_parse_attr_parse_if_invalid() {
+    let tokens = quote!(#[parse_if[something]]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[parse_if some_ident]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[parse_if]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[parse_if()]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_prefix_valid() {
+    let tokens = quote!(#[prefix(some_ident.some_func())]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Prefix(_),
+    ));
+}
+
+#[test]
+fn derive_parse_attr_prefix_invalid() {
+    let tokens = quote!(#[prefix[something]]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[prefix some_ident]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[prefix]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[prefix()]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
+
+#[test]
+fn derive_parse_attr_postfix_valid() {
+    let tokens = quote!(#[postfix(some_ident.some_func())]);
+    assert!(matches!(
+        parse2::<DeriveParseAttr>(tokens).unwrap(),
+        DeriveParseAttr::Postfix(_),
+    ));
+}
+
+#[test]
+fn derive_parse_attr_postfix_invalid() {
+    let tokens = quote!(#[postfix[something]]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[postfix some_ident]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[postfix]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+    let tokens = quote!(#[postfix()]);
+    assert!(parse2::<DeriveParseAttr>(tokens).is_err());
+}
